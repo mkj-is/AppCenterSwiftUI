283f216bc8690663eb04ced820022f9c2278deb2
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-10-12 21:30:19 +0200

    Initial Commit

---
+++ AppCenterSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AppCenterSwiftUI
+//
+//  Created by Matěj Kašpar Jirásek on 12/10/2019.
+//  Copyright © 2019 FUNTASTY Digital s.r.o. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
