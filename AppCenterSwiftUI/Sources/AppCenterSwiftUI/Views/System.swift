21a5d7f563c9f18dcf0b14ebc66f7bfc61884107
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-03-07 12:51:02 +0100

    Change OperatingSystem to OptionSet

--- AppCenterSwiftUI/Sources/AppCenterSwiftUI/Views/System.swift
+++ AppCenterSwiftUI/Sources/AppCenterSwiftUI/Views/System.swift
@@ -1,17 +1,22 @@
 import SwiftUI
 
-enum OperatingSystem {
-    case macOS, iOS, tvOS, watchOS
-
-    static var current: OperatingSystem {
+struct OperatingSystem: OptionSet {
+    let rawValue: UInt8
+    
+    static let macOS = OperatingSystem(rawValue: 1 << 0)
+    static let iOS = OperatingSystem(rawValue: 1 << 1)
+    static let tvOS = OperatingSystem(rawValue: 1 << 2)
+    static let watchOS = OperatingSystem(rawValue: 1 << 3)
+    
+    var isCurrent: Bool {
         #if os(macOS)
-        return .macOS
+        return contains(.macOS)
         #elseif os(iOS)
-        return .iOS
+        return contains(.iOS)
         #elseif os(tvOS)
-        return .tvOS
+        return contains(.tvOS)
         #elseif os(watchOS)
-        return .watchOS
+        return contains(.watchOS)
         #else
         #error("Compiling for unknown operating system.")
         #endif
@@ -28,7 +33,7 @@ struct System<Content: View>: View {
     }
 
     var body: some View {
-        if operatingSystem == .current {
+        if operatingSystem.isCurrent {
             content
         }
     }
